b637cfd472ba1fd346ebcd903dd9a32bf8718d07
NAVIN0507 <navinofficial2005@gmail.com>
2025-06-24 03:23:39 +0530

    added smallCalendar

--- src/components/SmallCalendar.jsx
+++ src/components/SmallCalendar.jsx
@@ -15,21 +15,33 @@ const SmallCalendar = () => {
       function handleNextMonth() {
         setcurrentMonthIdx(currentMonthIdx + 1);
       }
-      const {monthIndex}  = useContext(GlobalContext)
+      const {monthIndex , setSmallCalendarMonth , setDaySelected , daySelected}  = useContext(GlobalContext)
       useEffect(()=>{
         setcurrentMonthIdx(monthIndex)
       } , [monthIndex]);
-      
+      function getDayClass(day){
+        const format = "DD-MM-YY";
+        const today = dayjs().format(format);
+        const currday = day.format(format);
+        const slcDay = daySelected && daySelected.format(format);
+        if(today === currday)
+            return 'bg-blue-500 text-white rounded-full'
+        else if(currday === slcDay)
+            return "bg-blue-100 rounded-full text-blue-600 font-bold"
+        else return ""
+      }
   return (
     <div className='mt-9'>
         <header className='flex justify-between'>
             <p className='text-gray-500 font-bold'>{dayjs(new Date(dayjs().year() , currentMonthIdx)).format('MMMM YYYY')}</p>
-            <button className='cursor-pointer rounded-full hover:border border-gray-400 ease-in-out' onClick={handlePrevMonth}>
+           <div>
+           <button className='cursor-pointer rounded-full hover:border border-gray-400 ease-in-out' onClick={handlePrevMonth}>
           <ChevronLeft color='#000000'/>
         </button>
         <button className='mx-4 cursor-pointer rounded-full hover:border border-gray-400 ease-in-out' onClick={handleNextMonth}>
           <ChevronRight color='#000000'/>
         </button>
+           </div>
         </header>
      <div className='grid grid-cols-7 grid-rows-6 mt-7 '>
         {currentMonth[0].map((day , i)=>(
@@ -40,7 +52,12 @@ const SmallCalendar = () => {
         {currentMonth.map((row , i)=>(
             <React.Fragment key={i}>
                 {row.map((day , idx)=>(
-                    <button key={idx} className={`py-1 w-full`}>
+                    <button key={idx} className={`py-1 w-full ${getDayClass(day)} cursor-pointer`}
+                    onClick={()=>{
+                        setSmallCalendarMonth(currentMonthIdx)
+                        setDaySelected(day)
+                    }}
+                    >
                         <span className='text-sm'>{day.format('D')}</span>
                     </button>
                 ))}
