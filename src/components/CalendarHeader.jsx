6c7b147c57b8810b2a374074525bc4eae3d9c33d
NAVIN0507 <navinofficial2005@gmail.com>
2025-06-24 02:26:25 +0530

    added header

--- src/components/CalendarHeader.jsx
+++ src/components/CalendarHeader.jsx
@@ -1,8 +1,21 @@
+import { ChevronLeft, ChevronRight } from 'lucide-react'
 import React from 'react'
 
 const CalendarHeader = () => {
   return (
-    <div>CalendarHeader</div>
+    <header className='px-4 py-2 flex items-center'>
+        <img src="/calendar.jpg" alt="logo" className='mr-2 w-12 h-12'/>
+        <h1 className='mr-10  text-2xl text-gray-400 font-normal ml-2'>Calendar</h1>
+        <button className='border rounded py-2 px-4 mr-5 border-gray-400'>
+            Today
+        </button>
+        <button className='cursor-pointer rounded-full hover:border border-gray-400 ease-in-out'>
+          <ChevronLeft color='#000000'/>
+        </button>
+        <button className='mx-4 cursor-pointer rounded-full hover:border border-gray-400 ease-in-out'>
+          <ChevronRight color='#000000'/>
+        </button>
+    </header>
   )
 }
 
--- src/components/Day.jsx
+++ src/components/Day.jsx
@@ -1,12 +1,16 @@
+import dayjs from 'dayjs'
 import React from 'react'
 
 const Day = ({day , rowIdx}) => {
+    const getCurrentDateClass = ()=>{
+        return day.format("DD-MM-YY") === dayjs().format("DD-MM-YY") ?  'bg-blue-500 text-white rounded-full w-7' :''
+    }
   return (
     <div className='border border-gray-200 flex flex-col'>
         <header className='flex flex-col items-center'>
             {rowIdx===0 &&   <p className='text-sm mt-1'>{day.format('ddd').toUpperCase()}</p> }
           
-     <p className='text-sm p-1 my-1 text-center'>
+     <p className={`text-sm p-1 my-1 text-center ${getCurrentDateClass()}`}>
         {day.format('DD')}
      </p>
      </header>
